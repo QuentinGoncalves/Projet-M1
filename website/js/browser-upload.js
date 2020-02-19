74e41a17f0e20b957ea7e187e0be6bb42680014f
Leo-CADDEO <48925203+Leo-CADDEO@users.noreply.github.com>
2020-02-19 17:07:17 +0100

    Update browser-upload.js

--- website/js/browser-upload.js
+++ website/js/browser-upload.js
@@ -1,29 +1,28 @@
-var fileTypes = [
-  'audio/wav',
-  'audio/mp3',
-  'audio/ogg'
-]
 var file;
 var fileURL;
 var id_file;
 var id_process;
 var xml;
+var token = 'WyIxOSIsImRjODIxMDExZDBkYjY0YmNiZjZjNmIzZDQzODZhOTQwIl0.EJs66g.5qnm6VETk7EqY3ubO3SMgW-4gmQ';
 
 //Change in accordance with the HTML code
 var HTMLtranscription = document.getElementById('trans');
+var HTMLtranslation = document.getElementById('form21');
 var audio = document.getElementById('audio');
 var browser = document.getElementById('inputGroupFile01');
 
 browser.addEventListener('change', add_file);
+HTMLtranscription.style.overflow = "auto";
 
 function add_file() {
 	file = browser.files[0];
 	if(validFileType(file)){
+
 		//Set the audio into the HTML player
 		var blob = window.URL || window.webkitURL;
 	    if (!blob) {
 	        console.log('Your browser does not support Blob URLs :(');
-	        return;
+	        return;           
 	    }
 	    fileURL = blob.createObjectURL(file);
 		audio.src = fileURL;
@@ -33,10 +32,10 @@ function add_file() {
 		let formData = new FormData()
 		formData.append('file', file)
 		formData.append('content', '{"start": true, "asr_model_name": "french.studio.fr_FR"}')
-
+	
 		axios.post('http://lst-demo.univ-lemans.fr:8000/api/v1.1/files', formData, {
 		  headers: {
-		    'Authentication-Token' : 'WyIxOSIsImRjODIxMDExZDBkYjY0YmNiZjZjNmIzZDQzODZhOTQwIl0.EJs66g.5qnm6VETk7EqY3ubO3SMgW-4gmQ',
+		    'Authentication-Token' : token,
 		  }
 		})
 		.then(function (response) {
@@ -51,18 +50,16 @@ function add_file() {
 		  .catch(function (error) {
 		    console.log(error);
 		  });
-
+		  
 		}
 }
 
-//Return true if the file type is an audio .wav, .mp3 or .ogg
+//Return true if the file type is an audio file
 function validFileType(file) {
-  for(var i = 0; i < fileTypes.length; i++) {
-    if(file.type === fileTypes[i]) {
+  if(file.type.includes("audio/")) {
       return true;
-    }
   }
-  return true;
+  return false;
 }
 
 // Get the process and get the xml once finished
@@ -72,10 +69,10 @@ function getProcess(){
 	var progress;
 	axios({
 		    url: url,
-		    headers: {'Authentication-Token' : 'WyIxOSIsImRjODIxMDExZDBkYjY0YmNiZjZjNmIzZDQzODZhOTQwIl0.EJs66g.5qnm6VETk7EqY3ubO3SMgW-4gmQ' }
+		    headers: {'Authentication-Token' : token }
 			})
 			.then(function (reponse) {
-			    //On traite la suite une fois la réponse obtenue
+			    //On traite la suite une fois la réponse obtenue 
 			    console.log(reponse);
 			    if(reponse["data"]["status"] != "Finished"){
 			    	progress = reponse["data"]["progress"];
@@ -99,7 +96,7 @@ function getXML(){
 	var url = 'http://lst-demo.univ-lemans.fr:8000/api/v1.1/files/'+id_file+'/transcription?format=xml';
 	axios({
 		    url: url,
-		    headers: {'Authentication-Token' : 'WyIxOSIsImRjODIxMDExZDBkYjY0YmNiZjZjNmIzZDQzODZhOTQwIl0.EJs66g.5qnm6VETk7EqY3ubO3SMgW-4gmQ'}
+		    headers: {'Authentication-Token' : token}
 			})
 			.then(function (reponse) {
 			    //On traite la suite une fois la réponse obtenue
@@ -107,10 +104,10 @@ function getXML(){
 			    var parser = new DOMParser();
 			    xml = parser.parseFromString(reponse["data"],"text/xml");
 
-			    displayTranscription();
+			    displayTranscription();	//displayAllTranscription
 
 			    // Add event listener to the audio element (show dynamically the text)
-			    audio.addEventListener('timeupdate', displayTranscription, false);
+			    audio.addEventListener('timeupdate', displayTranscription, false);		//focusTranscription
 			    audio.addEventListener('loadedmetadata', removeAllListenerAudio, false);
 			})
 			.catch(function (erreur) {
@@ -118,6 +115,7 @@ function getXML(){
 			    console.log(erreur);
 			});
 }
+
 
 // Get the String text in the XML with a time
 function XMLtoString_CurrentTime(time){
@@ -135,18 +133,58 @@ function XMLtoString_CurrentTime(time){
 function displayTranscription(){
 	if(xml != null){
 		var duration = audio.currentTime;
+		//var result = string_difference(HTMLtranscription.value, XMLtoString_CurrentTime(duration));
+		//console.log(result[1]);
+		//add_translation(result[1]);
+
 		HTMLtranscription.innerHTML = XMLtoString_CurrentTime(duration);
+		HTMLtranscription.scrollTop = HTMLtranscription.scrollHeight;
+	}
+}
+
+function focusTranscription(){
+	if(xml != null){
+		var duration = audio.currentTime;
+		highlight(duration);
+	}
+}
+
+function highlight(time) {
+	displayAllTranscription();
+	var i = 0;
+	var index = 0;
+	var xmlTime = parseFloat(xml.getElementsByTagName("Word")[i].getAttribute("stime"));
+	while(xmlTime < time){
+		index = index + (xml.getElementsByTagName("Word")[i].childNodes[0].nodeValue.replace(/ /g,"") + " ").length;
+		i = i + 1;
+		xmlTime = parseFloat(xml.getElementsByTagName("Word")[i].getAttribute("stime"));
+	}
+	var text = xml.getElementsByTagName("Word")[i].childNodes[0].nodeValue.replace(/ /g,"") + " ";
+	var innerHTML = HTMLtranscription.innerHTML;
+    if (index >= 0) { 
+    	innerHTML = innerHTML.substring(0,index) + "<mark>" + innerHTML.substring(index,index+text.length) + "</mark>" + innerHTML.substring(index + text.length);
+    	HTMLtranscription.innerHTML = innerHTML;
+    }
+}
+
+function displayAllTranscription(){
+	if(xml != null){
+		var text = "";
+		for (var i = 0; i < xml.getElementsByTagName("Word").length; i++) {
+			text = text + xml.getElementsByTagName("Word")[i].childNodes[0].nodeValue.replace(/ /g,"") + " ";
+		}
+		HTMLtranscription.innerHTML = text;
 	}
 }
 
 function removeAllListenerAudio(){
 	HTMLtranscription.innerHTML = "";
-	audio.removeEventListener("timeupdate", displayTranscription, false);
+	audio.removeEventListener("timeupdate", displayTranscription, false);	//focusTranscription
 	audio.removeEventListener("loadedmetadata", removeAllListenerAudio, false);
 	var url = 'http://lst-demo.univ-lemans.fr:8000/api/v1.1/files/'+id_file;
 	axios.delete({
 		    url: url,
-		    headers: {'Authentication-Token' : 'WyIxOSIsImRjODIxMDExZDBkYjY0YmNiZjZjNmIzZDQzODZhOTQwIl0.EJs66g.5qnm6VETk7EqY3ubO3SMgW-4gmQ'}
+		    headers: {'Authentication-Token' : token}
 			})
 			.then(function (reponse) {
 			    //On traite la suite une fois la réponse obtenue
