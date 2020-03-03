e40c2136652c0c30a6f214276d37dd6fa46a32b4
Leo-CADDEO <48925203+Leo-CADDEO@users.noreply.github.com>
2020-03-03 18:14:34 +0100

    Update save.php

--- website/save.php
+++ website/save.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+
+ini_set('upload_max_filesize', '10M');
+ini_set('post_max_size', '10M');
+ini_set('max_input_time', 300);
+ini_set('max_execution_time', 300);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_FILES['file'])) {
         $errors = [];
@@ -8,17 +14,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $file_tmp = $_FILES['file']['tmp_name'];
         $file_type = $_FILES['file']['type'];
         $file_size = $_FILES['file']['size'];
-        $file_ext = strtolower(end(explode('.', $_FILES['file']['name'])));
+        //$file_ext = strtolower(end(explode('.', $_FILES['file']['name'])));
 
         $file = $path . $file_name . '.mp3';
 
-        if ($file_size > 2097152) {
-            $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
-        }
-
         if (empty($errors)) {
             move_uploaded_file($file_tmp, $file);
-            //print_r("Success");
+            print_r("Success");
         }
 
         if ($errors) print_r($errors);
