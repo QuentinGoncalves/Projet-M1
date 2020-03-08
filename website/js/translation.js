1dfc3b302bbcfb85cae62c87239b917f31320127
Leo-CADDEO <48925203+Leo-CADDEO@users.noreply.github.com>
2020-03-08 18:43:17 +0100

    Update translation.js

--- website/js/translation.js
+++ website/js/translation.js
@@ -1,10 +1,14 @@
 var HTMLtranslation = document.getElementById('trad');
+var Input = document.getElementById('servers');
+var Output = document.getElementById('ChoixSortie');
+var source = "en";
+var target = "fr";
 var port = 8083;
 
 function add_translation(text){
-	//text = "hello";
+	set_language();
 	let formData = new FormData();
-	var url = 'http://lst-demo.univ-lemans.fr:'+port+'/translate?q=%22'+encodeURIComponent(text)+'%22&source=en&target=fr&key=bla';
+	var url = 'http://lst-demo.univ-lemans.fr:'+port+'/translate?q=%22'+encodeURIComponent(text)+'%22&source='+source+'&target='+target+'&key=bla';
 	var translated = "";
 	if(text != "" && text != undefined){
 		axios.get(url, formData, {
@@ -15,17 +19,27 @@ function add_translation(text){
 				var string = list[0]['translatedTextRaw'];
 				var tab = string.split('"');
 				translated = tab[(tab.length-1)].replace(/&|nbsp|;|!|"|[{()}]/g,"") + " ";
-				while(translated.charAt(0) == " "){
+				while(translated != "" && translated.charAt(0) == " "){
 					translated = translated.substring(1);
 				}
 				HTMLtranslation.value = HTMLtranslation.value + translated;
 			  })
 			  .catch(function (error) {
+			  	HTMLtranslation.value = "Erreur: " + error;
 			    console.log(error);
 			  });
 	}
 	//console.log(translated);
 	//return translated;
+}
+
+function delete_translation(){
+	HTMLtranslation.value = "";
+}
+
+function set_language(){
+	source = Input.options[Input.selectedIndex].getAttribute("abbreviation");
+	target = Output.options[Output.selectedIndex].getAttribute("abbreviation");
 }
 
 // Retourne la difference entre deux string
